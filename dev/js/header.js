130c3461e3ae7da96e01bfd8e52e37d0db1cb45e
Sigrid Steinholt Bygdås <Sigrid.Bygdas@halogen.no>
2017-05-18 11:25:02 +0200

    Adjust menu styles

--- dev/js/header.js
+++ dev/js/header.js
@@ -1,25 +1,22 @@
-const mobileMenu  = () => {
+const menuEvents  = () => {
   const $ = jQuery.noConflict();
 
   const $menuContainer = $('.h-Menu_Container');
-  const $menuBtn = $('.h-Menu_Trigger-mobile');
-  $menuBtn.on('click', () => {
-    $menuBtn.toggleClass('active');
+  const $mobileMenuBtn = $('.h-Menu_Trigger-mobile');
+  const $subMenuBtn = $('.h-Menu_Trigger');
+  $mobileMenuBtn.on('click', () => {
+    $mobileMenuBtn.toggleClass('active');
     $menuContainer.toggleClass('h-Menu_Container-open');
-    if ($menuContainer.hasClass('h-Menu_Container-open')) {
-      $('.h-Menu_Trigger').on('click', (event) => {
-        const $selectedMenu = $(event.target).parent('.h-Menu');
-        if ($selectedMenu.hasClass('h-Menu-open')) { // menu is open
-          $selectedMenu.removeClass('h-Menu-open'); // close it
-        } else {
-          $('.h-Menu-open').removeClass('h-Menu-open'); // close the other menu if open
-          $selectedMenu.addClass('h-Menu-open'); // open selected menu
-        }
-      });
+  });
+  $subMenuBtn.on('click', (event) => {
+    const $selectedMenu = $(event.target).parent('.h-Menu');
+    if ($selectedMenu.hasClass('h-Menu-open')) { // menu is open
+      $selectedMenu.removeClass('h-Menu-open'); // close it
     } else {
-      $('.h-Menu_Trigger').off('click');
+      $('.h-Menu-open').removeClass('h-Menu-open'); // close the other menu if open
+      $selectedMenu.addClass('h-Menu-open'); // open selected menu
     }
   });
 };
 
-mobileMenu();
+menuEvents();
