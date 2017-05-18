aa6f4a985ca5f091c4ba6f665cda4d6b5c7fb6ff
Aleksy Goroszko <aleksy.goroszko@gmail.com>
2017-05-18 16:43:54 +0200

    Remove deploy taks as a part of build task

--- src/grunt/tasks/build.js
+++ src/grunt/tasks/build.js
@@ -1,21 +1,21 @@
 module.exports = function(grunt) {
-	grunt.registerTask('build', [
+  grunt.registerTask('build', [
 
-		'clean:build_sass',
-		'clean:build_fonts',
-		'clean:build_img',
-		'clean:build_js',
-		'clean:build_html'
+    'clean:build_sass',
+    'clean:build_fonts',
+    'clean:build_img',
+    'clean:build_js',
+    'clean:build_html'
 
-		// PROD
-		, 'copy:build_fonts', 'copy:build_img', 'copy:build_sections', 'sass:build', 'concat:build'
+    // PROD
+    , 'copy:build_fonts', 'copy:build_img', 'copy:build_sections', 'sass:build', 'concat:build'
 
-		// Minify CSS
-		, 'cssmin:build', 'copy:build_css'
-		// Minify JS
-		, 'uglify:build'
-		// BUILD PROD HTML
-		, 'htmlbuild:build', 'ftp-deploy:build'
+    // Minify CSS
+    , 'cssmin:build', 'copy:build_css'
+    // Minify JS
+    , 'uglify:build'
+    // BUILD PROD HTML
+    , 'htmlbuild:build'
 
-	]);
+  ]);
 };
