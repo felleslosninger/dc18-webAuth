8f2677b329937b3ac1df98bd4297b8cb655fadb0
Sigrid Steinholt Bygdås <Sigrid.Bygdas@halogen.no>
2017-05-24 16:32:28 +0200

    eID-0D: Country Selector added

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 
 import test from './test/test';
 import menuEvents from './sections/header';
+import countrySelector from './sections/country-selector'
 
 test();
 menuEvents();
+countrySelector();
