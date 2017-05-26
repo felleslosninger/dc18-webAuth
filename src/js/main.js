b9fc5f70f1fad7554fb8fed8c231ed3e2f136f5d
Sigrid Steinholt Bygdås <Sigrid.Bygdas@halogen.no>
2017-05-26 14:27:55 +0200

    Add eID-selektor-0E

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@
 import menuEvents from './sections/header';
 import tab from './sections/tab';
 import countrySelector from './sections/country-selector'
+import disabledLinks from './sections/disabled-links'
 
 menuEvents();
 tab();
 countrySelector();
+disabledLinks();
