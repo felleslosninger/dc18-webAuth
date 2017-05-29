38050d32f734a875af5c4b793d1f0e5486103425
Sigrid Steinholt Bygdås <Sigrid.Bygdas@halogen.no>
2017-05-29 13:36:02 +0200

    eID-selektor-0C

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ import menuEvents from './sections/header';
 import tab from './sections/tab';
 import countrySelector from './sections/country-selector'
 import disabledLinks from './sections/disabled-links'
+import accordion from './sections/accordion'
 
 menuEvents();
 tab();
 countrySelector();
 disabledLinks();
+accordion();
---
+++ src/js/sections/accordion.js
@@ -0,0 +1,27 @@
+const accordion = () => {
+
+  const $ = jQuery.noConflict();
+
+  $('.la-List_Element').on('click', (e) => {
+    e.preventDefault();
+    let $target = $(e.currentTarget);
+    let $list = $(document.getElementById($target.attr('aria-controls')));
+    // addAnimation class used to prevent animation on page load. Safe to add now:
+    $target.find('.fa-angle-down').addClass('addAnimation');
+    // There are two animations. They must run simultaneously:
+    if ($list.hasClass('open')) {
+      // about to close, aria attribute is updated and this triggers arrow animation
+      $target.attr('aria-expanded', 'false');
+    } else {
+      // about to open, aria attribute is updated and this triggers arrow animation
+      $target.attr('aria-expanded', 'true');
+    }
+    // List open / close with animation
+    $list.slideToggle(300, () => {
+      $list.toggleClass('open');
+    });
+
+  });
+};
+
+export default accordion;
