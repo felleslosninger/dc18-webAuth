908b21f52e94ebecef9ef8007e111a9cd94e41a0
Aleksy Goroszko <aleksy.goroszko@gmail.com>
2017-05-29 15:25:42 +0200

    Generate build

--- build/js/script.build.js
+++ build/js/script.build.js
@@ -1,20 +1,332 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _test = require('./test/test');
-
-var _test2 = _interopRequireDefault(_test);
-
 var _header = require('./sections/header');
 
 var _header2 = _interopRequireDefault(_header);
 
+var _tab = require('./sections/tab');
+
+var _tab2 = _interopRequireDefault(_tab);
+
+var _countrySelector = require('./sections/country-selector');
+
+var _countrySelector2 = _interopRequireDefault(_countrySelector);
+
+var _disabledLinks = require('./sections/disabled-links');
+
+var _disabledLinks2 = _interopRequireDefault(_disabledLinks);
+
+var _accordion = require('./sections/accordion');
+
+var _accordion2 = _interopRequireDefault(_accordion);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _test2.default)();
 (0, _header2.default)();
+(0, _tab2.default)();
+(0, _countrySelector2.default)();
+(0, _disabledLinks2.default)();
+(0, _accordion2.default)();
 
-},{"./sections/header":2,"./test/test":3}],2:[function(require,module,exports){
+},{"./sections/accordion":2,"./sections/country-selector":3,"./sections/disabled-links":4,"./sections/header":5,"./sections/tab":6}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var accordion = function accordion() {
+
+  var $ = jQuery.noConflict();
+
+  $('.la-List_Element').on('click', function (e) {
+    e.preventDefault();
+    var $target = $(e.currentTarget);
+    var $list = $(document.getElementById($target.attr('aria-controls')));
+    // addAnimation class used to prevent animation on page load. Safe to add now:
+    $target.find('.fa-angle-down').addClass('addAnimation');
+    // There are two animations. They must run simultaneously:
+    if ($list.hasClass('open')) {
+      // about to close, aria attribute is updated and this triggers arrow animation
+      $target.attr('aria-expanded', 'false');
+    } else {
+      // about to open, aria attribute is updated and this triggers arrow animation
+      $target.attr('aria-expanded', 'true');
+    }
+    // List open / close with animation
+    $list.slideToggle(300, function () {
+      $list.toggleClass('open');
+    });
+  });
+};
+
+exports.default = accordion;
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var SearchList = function () {
+	function SearchList() {
+		_classCallCheck(this, SearchList);
+
+		var $ = jQuery.noConflict();
+
+		this.inputField = $('input[type=text]');
+		this.countrycode = $('input[type=hidden]');
+		this.inputList = $('#js-country-list');
+		this.trigger = $('#js-trigger');
+		this.errorHandler = $('#js-country');
+		this.listElements = this.inputList.find('li');
+		this.current = 0;
+	}
+
+	_createClass(SearchList, [{
+		key: 'addEvents',
+		value: function addEvents() {
+
+			var $ = jQuery.noConflict();
+			var self = this;
+
+			$('form').submit(function (e) {
+				if (self.countrycode.val().length === 0) {
+					e.preventDefault();
+					self.checkInput(self.inputField.val());
+				}
+			});
+
+			self.inputField.on('keydown', function () {
+				self.errorHandler.removeClass('error');
+				if (e.keyCode === 9) {
+					return true;
+				}
+				if (!self.inputList.hasClass('open')) {
+					self.openMenu();
+				}
+			});
+
+			self.inputField.on('keyup click input', function (e) {
+
+				if (e.keyCode === 27) {
+					// Escape, close inputList if open
+					e.preventDefault();
+					if (self.inputList.hasClass('open')) {
+						self.closeMenu();
+					}
+					return true;
+				} else if (e.keyCode === 38) {
+					// Up arrow, go to last element in list if list open
+					e.preventDefault();
+					if (self.inputList.hasClass('open')) {
+						$.each(self.listElements, function (j) {
+							if ($(this).is(':visible')) {
+								$(this).find('> a').focus();
+								self.current = j;
+							}
+						});
+					}
+					return true;
+				} else if (e.keyCode === 40) {
+					// Down arrow, go to first element in list if list open
+					e.preventDefault();
+					if (self.inputList.hasClass('open')) {
+						var found = false;
+						$.each(self.listElements, function (i) {
+							if ($(this).is(':visible') && !found) {
+								$(this).find('> a').focus();
+								found = true;
+								self.current = i;
+							}
+						});
+					}
+					return true;
+				}
+				if (this.value.length > 0) {
+					self.listElements.filter(function () {
+						var listElm = $(this).find('a').html().toLowerCase();
+						var typedText = self.inputField.val().toLowerCase();
+						if (listElm.indexOf(typedText) === 0) {
+							$(this).show();
+							return true;
+						} else {
+							$(this).hide();
+							return false;
+						}
+					});
+				} else {
+					self.listElements.show();
+				}
+			});
+
+			self.trigger.on('click', function (e) {
+				e.preventDefault();
+				if (!self.inputList.hasClass('open')) {
+					self.openMenu();
+				} else {
+					self.closeMenu();
+				}
+			});
+
+			self.inputList.find('a').on('click', function (e) {
+				e.preventDefault();
+				self.inputField.val($(this).html());
+				self.countrycode.val($(this).attr('data-id'));
+				self.closeMenu();
+			});
+
+			self.inputList.find('a').on('keydown', function (e) {
+				if (e.keyCode === 38) {
+					e.preventDefault();
+					var newCurrent = -1;
+					var toFocus = null;
+					$.each(self.listElements, function (i) {
+						if ($(this).is(':visible') && i < self.current) {
+							newCurrent = i;
+							toFocus = $(this);
+						}
+					});
+					if (newCurrent === -1) {
+						$.each(self.listElements, function (j) {
+							if ($(this).is(':visible')) {
+								$(this).find('> a').focus();
+								self.current = j;
+							}
+						});
+					} else {
+						if (toFocus !== null) {
+							toFocus.find('> a').focus();
+							self.current = newCurrent;
+						}
+					}
+				} else if (e.keyCode === 40) {
+					e.preventDefault();
+					var _newCurrent = -1;
+					var _toFocus = null;
+					var found = false;
+					$.each(self.listElements, function (i) {
+						if ($(this).is(':visible') && i > self.current && !found) {
+							_newCurrent = i;
+							_toFocus = $(this);
+							found = true;
+						}
+					});
+					if (_newCurrent === -1) {
+						// start from top
+						self.current = 0;
+						found = false;
+						$.each(self.listElements, function (i) {
+							if ($(this).is(':visible') && !found) {
+								$(this).find('> a').focus();
+								found = true;
+								self.current = i;
+							}
+						});
+					} else {
+						_toFocus.find('> a').focus();
+						self.current = _newCurrent;
+					}
+				} else if (e.keyCode === 9) {
+					e.preventDefault();
+					self.inputField.val($(this).html());
+					self.closeMenu();
+					$('.button-holder').find('> .submit').focus();
+				}
+			});
+
+			$(document).on('click', function (e) {
+				if (!document.getElementById('js-country').contains(e.target) && !document.getElementById('js-country-list').contains(e.target)) {
+					self.closeMenu();
+				}
+			});
+
+			$("form").submit(function (e) {
+				if (self.countrycode.val().length === 0) {
+					e.preventDefault();
+					self.checkInput(self.inputField.val());
+				}
+			});
+		}
+	}, {
+		key: 'openMenu',
+		value: function openMenu() {
+			var $ = jQuery.noConflict();
+
+			this.inputList.slideDown(200, function () {
+				$(this).addClass('open');
+			});
+		}
+	}, {
+		key: 'closeMenu',
+		value: function closeMenu() {
+			var $ = jQuery.noConflict();
+
+			this.inputList.slideUp(200, function () {
+				$(this).removeClass('open');
+			});
+		}
+	}, {
+		key: 'checkInput',
+		value: function checkInput(name) {
+			var $ = jQuery.noConflict();
+			var self = this;
+			var countryId = null;
+			$.each(self.listElements, function (i) {
+				if (countryId === null) {
+					var countryElm = $(this).find('> a');
+					if (countryElm.html().toLowerCase() === name.trim().toLowerCase()) {
+						countryId = countryElm.attr('data-id');
+						self.countrycode.val(countryId);
+						return true;
+					}
+				}
+			});
+			if (countryId === null) {
+				this.errorHandler.addClass('error');
+			}
+		}
+	}]);
+
+	return SearchList;
+}();
+
+;
+
+var countrySelector = function countrySelector() {
+
+	var $ = jQuery.noConflict();
+
+	if ($('#js-country-list').length > 0) {
+		var searchCtrl = new SearchList();
+		searchCtrl.addEvents();
+	}
+};
+
+exports.default = countrySelector;
+
+},{}],4:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var disabledLinks = function disabledLinks() {
+  var $ = jQuery.noConflict();
+
+  $('a.disabled').on('click', function (e) {
+    e.preventDefault();
+  });
+};
+
+exports.default = disabledLinks;
+
+},{}],5:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -53,13 +365,24 @@ var menuEvents = function menuEvents() {
 
 exports.default = menuEvents;
 
-},{}],3:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 'use strict';
 
-var test = function test() {
-  console.log('test function');
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var menuEvents = function menuEvents() {
+  var $ = jQuery.noConflict();
+
+  $('.tb-Header a').on('click', function (event) {
+    event.preventDefault();
+
+    var $el = $(event.currentTarget);
+    var $target = $el.data('target');
+    $el.addClass('active').siblings().removeClass('active').end().closest('.tb-Container').find('.tb-Tab').hide().end().find('[data-id=' + $target + ']').show();
+  });
 };
 
-module.exports = test;
+exports.default = menuEvents;
 
 },{}]},{},[1]);
