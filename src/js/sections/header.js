4493475b398e62163f0bbe3bcb3817707dd55e9d
Sigrid Steinholt Bygdås <Sigrid.Bygdas@halogen.no>
2017-05-30 11:24:30 +0200

    Add MinID1 menu Add aria attributes to main menues

--- src/js/sections/header.js
+++ src/js/sections/header.js
@@ -4,24 +4,49 @@ const menuEvents  = () => {
   const $menuContainer = $('.h-Menu_Container');
   const $mobileMenuBtn = $('.h-Menu_Trigger-mobile');
   const $subMenuBtn = $('.h-Menu_Trigger');
+  const $lastElement = $('.js-last');
+
   $mobileMenuBtn.on('click', (event) => {
     event.preventDefault();
     $mobileMenuBtn.toggleClass('active');
     $menuContainer.toggleClass('h-Menu_Container-open');
   });
+
   $subMenuBtn.on('click', (event) => {
     event.preventDefault();
-    const $selectedMenu = $(event.currentTarget).parent('.h-Menu');
+    const $currentTrigger = $(event.currentTarget);
+    const $selectedMenu = $currentTrigger.parent('.h-Menu');
     if ($selectedMenu.hasClass('h-Menu-open')) { // menu is open
       $selectedMenu.removeClass('h-Menu-open'); // close it
+      $currentTrigger.attr('aria-expanded', 'false');
     } else {
+      $('.h-Menu-open').find('.h-Menu_Trigger').attr('aria-expanded', 'false');
       $('.h-Menu-open').removeClass('h-Menu-open'); // close the other menu if open
       $selectedMenu.addClass('h-Menu-open'); // open selected menu
+      $currentTrigger.attr('aria-expanded', 'true');
     }
   });
+
+  $subMenuBtn.on('keydown', (event) => {
+    if (event.keyCode === 9 && event.shiftKey ) { // Shift+tab, go to last element in list if list open
+      event.preventDefault();
+      if ($(event.currentTarget).parent('.h-Menu').hasClass('h-Menu-open')) {
+        $lastElement.focus();
+      }
+    }
+  });
+
+  $lastElement.on('keydown', (event) => {
+    if (event.keyCode === 9 && !event.shiftKey) { // Tab, go to menu trigger
+      event.preventDefault();
+      $('.h-Menu-open').find('.h-Menu_Trigger').focus();
+    }
+  });
+
   $(document).on('click', (event) => {
     if (!(document.getElementById('js-menues').contains(event.target))) {
       if (!($(event.currentTarget).hasClass('h-Menu_Trigger-mobile'))) {
+        $('.h-Menu_Trigger').attr('aria-expanded', 'false');
         $('.h-Menu').removeClass('h-Menu-open');
       }
     }
---
+++ src/js/sections/minid-menu.js
@@ -0,0 +1,42 @@
+const minidMenu = () => {
+  const $ = jQuery.noConflict();
+
+  const $miMenu = $('.mi-Menu');
+  const $miMenuTrigger = $('.mi-Menu_Trigger');
+  const $lastMenuElement = $miMenu.find('.js-last');
+
+  $miMenuTrigger.on('click', (e) => {
+    e.preventDefault();
+    $miMenu.toggleClass('mi-Menu-open');
+    if ($miMenu.hasClass('mi-Menu-open')) {
+      $miMenuTrigger.attr('aria-expanded', 'true');
+    } else {
+      $miMenuTrigger.attr('aria-expanded', 'false');
+    }
+  });
+
+  $miMenuTrigger.on('keydown', (e) => {
+    if (e.keyCode === 9 && e.shiftKey ) { // Shift+tab, go to last element in list if list open
+      e.preventDefault();
+      if ($miMenu.hasClass('mi-Menu-open')) {
+        $lastMenuElement.focus();
+      }
+    }
+  });
+
+  $lastMenuElement.on('keydown', (e) => {
+    if (e.keyCode === 9 && !e.shiftKey) { // Tab, go to menu trigger
+      e.preventDefault();
+      $miMenuTrigger.focus();
+    }
+  });
+
+  $(document).on('click', (event) => {
+    if (!(document.getElementById('js-mi-menu').contains(event.target))) {
+      $miMenuTrigger.attr('aria-expanded', 'false');
+      $miMenu.removeClass('mi-Menu-open');
+    }
+  });
+};
+
+export default minidMenu;
