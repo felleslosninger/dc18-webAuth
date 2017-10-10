11ed9d8a33253e1fa7053e41f3a967b1ad019d14
Sigrid Steinholt Bygdås <Sigrid.Bygdas@halogen.no>
2017-10-10 15:14:17 +0200

    Enable save button when checkbox checked Add icons as svg

---
+++ src/js/save-enabler.js
@@ -0,0 +1,17 @@
+const saveEnabler = () => {
+
+  const $ = jQuery.noConflict();
+
+  $('.fm-Checkbox input').on('change', (e) => {
+    let $checkbox = $(e.currentTarget);
+    if ($checkbox.prop('checked')) {
+      $('.btn-Action').prop('disabled', false);
+    }
+    else {
+      $('.btn-Action').prop('disabled', true);
+    }
+  });
+
+};
+
+saveEnabler();
