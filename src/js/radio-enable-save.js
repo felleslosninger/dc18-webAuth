cf989cb2a5c784cdfb459a086af5862ac6012435
Sigrid Steinholt Bygdås <Sigrid.Bygdas@halogen.no>
2017-10-11 09:15:33 +0200

    Add script for enabling action button on radio button select

---
+++ src/js/radio-enable-save.js
@@ -0,0 +1,14 @@
+const radioEnableSave = () => {
+
+  const $ = jQuery.noConflict();
+
+  $('.fm-RadioButtons input').on('change', (e) => {
+    let $radioBtn = $(e.currentTarget);
+    if ($radioBtn.prop('checked')) {
+      $('.btn-Action').prop('disabled', false);
+    }
+  });
+
+};
+
+radioEnableSave();
