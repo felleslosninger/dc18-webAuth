7997eb15b59b6a34b9e06ee2404b85c7828b1db8
Tharmapalan <camp-tth@DIFI.no>
2018-06-15 12:19:26 +0200

    Fix for basic setup

--- src/main/java/no/difi/controllers/MainController.java
+++ src/main/java/no/difi/controllers/MainController.java
@@ -1,8 +1,5 @@
 package no.difi.controllers;
 
-import no.difi.models.Customer;
-import no.difi.models.CustomerDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +9,6 @@ import java.security.Principal;
 @Controller
 public class MainController {
 
-    @Autowired
-    private CustomerDAO customerDAO;
-
     @GetMapping(path="/")
     public String index() {
         return "external";
@@ -23,31 +17,12 @@ public class MainController {
     @GetMapping(path="/customers")
     public String customers(Principal principal, Model model) {
         addCustomers();
-        Iterable<Customer> customers = customerDAO.findAll();
-        model.addAttribute("customers", customers);
+        model.addAttribute("customers", "customers");
         model.addAttribute("username", principal.getName());
         return "customers";
     }
 
     public void addCustomers() {
-
-        Customer customer1 = new Customer();
-        customer1.setAddress("1111 foo blvd");
-        customer1.setName("Foo Industries");
-        customer1.setServiceRendered("Important services");
-        customerDAO.save(customer1);
-
-        Customer customer2 = new Customer();
-        customer2.setAddress("2222 bar street");
-        customer2.setName("Bar LLP");
-        customer2.setServiceRendered("Important services");
-        customerDAO.save(customer2);
-
-        Customer customer3 = new Customer();
-        customer3.setAddress("33 main street");
-        customer3.setName("Big LLC");
-        customer3.setServiceRendered("Important services");
-        customerDAO.save(customer3);
     }
 
 }
--- src/main/java/no/difi/models/CustomerDAO.java
+++
@@ -1,7 +0,0 @@
-package no.difi.models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerDAO extends CrudRepository<Customer, Long> {
-
-}
