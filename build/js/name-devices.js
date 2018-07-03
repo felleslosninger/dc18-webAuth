8edd04cdebfe94468fb357be335851620f6700bf
ingrskar <ingrskar@gmail.com>
2018-07-03 11:57:40 +0200

    chnaged layout for devices

--- build/js/name-devices.js
+++ build/js/name-devices.js
@@ -1,8 +1,18 @@
 function addItem(){
-    var li = document.createElement("LI");
-    var input = document.getElementById("add");
-    li.innerHTML = input.value;
-    input.value = "";
+    const li = document.createElement("LI");
+    const input = document.getElementById("add");
+    const name = input.value;
+
+    var dt = new Date();
+    var utcDate = dt.toUTCString();
+
+    li.innerHTML = "<a class='ln-List_Element'> " +
+        "<div class='ln-List_Element_Service'> " +
+        "<span class='ln-List_Element_Service_Name'>"+ name +"</span> " +
+        "<span class='ln-List_Element_Service_Info'>Registert: "+ utcDate +"</span> " +
+        "</div> " +
+        "<span class='fa fa-angle-right fa-lg'></span> " +
+        "</a>";
 
     document.getElementById("devices").appendChild(li);
 }
