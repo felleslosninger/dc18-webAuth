3f830034aaef84c48f662173ba07f5fef54683dd
viktorfa <vikfand@gmail.com>
2018-07-04 14:28:30 +0200

    Make functionality to remove device from local storage.

--- build/js/addWebauthnDeviceMenu.js
+++ build/js/addWebauthnDeviceMenu.js
@@ -1,7 +1,6 @@
 const addWebauthnDeviceMenu = () => {
   const $ = jQuery.noConflict();
 
-
   $(document).on('webauthn:register-success', (event) => {
     event.preventDefault();
     $('#webauthn-added-device').toggle();
@@ -11,14 +10,16 @@ const addWebauthnDeviceMenu = () => {
     $('#webauthn-add-device-box').toggle();
   });
 
+
   $('#undo-webauthn-device-btn').on('click', (event) => {
     event.preventDefault();
+    $(document).trigger('webauthn:remove-device', {});
     $('#webauthn-hide-warning').toggle();
     $('#add-webauthn-message').show();
     $('#add-webauthn-message').text('Smartenheten er fjernet');
     $('#webauthn-added-device').toggle();
     $('#webauthn-add-device-box').toggle();
-  })
+  });
 };
 
 
--- build/js/localStorage.js
+++ build/js/localStorage.js
@@ -1,12 +1,17 @@
-
-// Store
 const localStorage = () => {
-    const $ = jQuery.noConflict();
-    $(document).on('webauthn:register-success', (event, data) => {
-        console.log('test')
-        window.localStorage.setItem("webauthn-device", JSON.stringify(data));
-    });
+  const $ = jQuery.noConflict();
+
+  $(document).on('webauthn:register-success', (event, data) => {
+    console.log('webauthn:register-success in localStorage.js');
+    window.localStorage.setItem("webauthn-device", JSON.stringify(data));
+  });
+
+  $(document).on('webauthn:remove-device', (event) => {
+    event.preventDefault();
+    window.localStorage.removeItem('webauthn-device');
+    console.log('webauthn-device cleared from local storage');
+  });
+
 };
 
 localStorage();
-
