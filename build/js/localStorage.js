aa184989ddf5ae689049eae9b2a93ec78a54f6ae
Thay <thayanan@gmail.com>
2018-07-04 13:28:36 +0200

    Add local storage

--- build/js/localStorage.js
+++ build/js/localStorage.js
@@ -5,8 +5,8 @@ const localStorage = () => {
     $(document).on('webauthn:register-success', (event, data) => {
         console.log('test')
         window.localStorage.setItem("webauthn-device", JSON.stringify(data));
-    })
-}
+    });
+};
 
 localStorage();
 
