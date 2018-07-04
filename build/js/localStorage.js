6bb7349357f6d5d3947113b4235776432595fe3e
Thay <thayanan@gmail.com>
2018-07-04 21:01:23 +0200

    Clean js code

--- build/js/localStorage.js
+++ build/js/localStorage.js
@@ -1,16 +1,16 @@
 const localStorage = () => {
-  const $ = jQuery.noConflict();
+    const $ = jQuery.noConflict();
 
-  $(document).on('webauthn:register-success', (event, data) => {
-    console.log('webauthn:register-success in localStorage.js');
-    window.localStorage.setItem("webauthn-device", JSON.stringify(data));
-  });
+    $(document).on('webauthn:register-success', (event, data) => {
+        console.log('webauthn:register-success in localStorage.js');
+        window.localStorage.setItem("webauthn-device", JSON.stringify(data));
+    });
 
-  $(document).on('webauthn:remove-device', (event) => {
-    event.preventDefault();
-    window.localStorage.removeItem('webauthn-device');
-    console.log('webauthn-device cleared from local storage');
-  });
+    $(document).on('webauthn:remove-device', (event) => {
+        event.preventDefault();
+        window.localStorage.removeItem('webauthn-device');
+        console.log('webauthn-device cleared from local storage');
+    });
 
 };
 
