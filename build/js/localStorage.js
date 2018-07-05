7d1c890201573f8831c044080424ba435ca87abf
Thay <thayanan@gmail.com>
2018-07-05 12:18:24 +0200

    Clean code

--- build/js/localStorage.js
+++ build/js/localStorage.js
@@ -1,30 +1,30 @@
 const localStorage = () => {
-  const $ = jQuery.noConflict();
+    const $ = jQuery.noConflict();
 
-  /**
-   * Saves a Webauthn device to local storage.
-   */
-  $(document).on('webauthn:register-success', (event, data) => {
-    console.log('webauthn:register-success in localStorage.js');
-    window.localStorage.setItem("webauthn-device", JSON.stringify(data));
-  });
+    /**
+     * Saves a Webauthn device to local storage.
+     */
+    $(document).on('webauthn:register-success', (event, data) => {
+        console.log('webauthn:register-success in localStorage.js');
+        window.localStorage.setItem("webauthn-device", JSON.stringify(data));
+    });
 
-  /**
-   * Removes the Webauthn device from local storage and changes settings so that Webauthn is not the preferred 2fa.
-   */
-  $(document).on('webauthn:remove-device', (event) => {
-    event.preventDefault();
-    window.localStorage.removeItem('webauthn-device');
-    console.log('webauthn-device cleared from local storage');
-    resetPreferredAuthType();
-  });
+    /**
+     * Removes the Webauthn device from local storage and changes settings so that Webauthn is not the preferred 2fa.
+     */
+    $(document).on('webauthn:remove-device', (event) => {
+        event.preventDefault();
+        window.localStorage.removeItem('webauthn-device');
+        console.log('webauthn-device cleared from local storage');
+        resetPreferredAuthType();
+    });
 
-  /**
-   * Function to be called after the Webauthn device is removed.
-   */
-  const resetPreferredAuthType = () => {
-    window.localStorage.removeItem('auth-type');
-  };
+    /**
+     * Function to be called after the Webauthn device is removed.
+     */
+    const resetPreferredAuthType = () => {
+        window.localStorage.removeItem('auth-type');
+    };
 
 };
 
