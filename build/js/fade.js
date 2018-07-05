2138ff247c35e7502c339fb3ab0106e5e52eb41d
Thay <thayanan@gmail.com>
2018-07-05 09:56:16 +0200

    Fix fade animation bug

--- build/js/fade.js
+++ build/js/fade.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     $(".background-image").hide(0).delay(0).fadeIn(2500);
     $("#loginBox").hide(0).delay(0).fadeIn(2800);
+    $("#loginText").hide(0).delay(0).fadeIn(3000);
+    $(".w3-btn w3-black").hide(0).delay(0).fadeIn(3200);
 });
