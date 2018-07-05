9037ce8f5b14aa0141a59e662beb2deda6967846
Itland <camp-vit@DIFI.no>
2018-07-05 12:25:55 +0200

    Fix 'Avbryt' and 'Tilbake' links for onboarding (#35)

---
+++ build/js/abort-buttons.js
@@ -0,0 +1,11 @@
+(function() {
+    let $ = jQuery.noConflict();
+    $(".btn-Abort.partially").on("click", e => {
+        e.preventDefault();
+        window.location.href = document.referrer;
+    });
+    $(".btn-Abort.fully").on("click", e => {
+        e.preventDefault();
+        window.location.href = "../landing-page/index.html";
+    });
+})();
