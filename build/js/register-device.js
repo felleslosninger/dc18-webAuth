ea17698a9ea7e0af66712d05b30546eeafee2d02
Thay <thayanan@gmail.com>
2018-07-06 09:34:35 +0200

    Add dynamic save button

--- build/js/register-device.js
+++ build/js/register-device.js
@@ -3,6 +3,7 @@ const $ = jQuery.noConflict();
 function hideInfo() {
     $(document).on('webauthn:register-success', (event, data) => {
         $('#webauthn-info').hide();
+        $('#nextbtn').html("Lagre");
     })
 }
 
