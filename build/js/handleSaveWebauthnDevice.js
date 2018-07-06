7a6593007d4f057a3f614a22836db4a2993898c7
viktorfa <vikfand@gmail.com>
2018-07-06 09:41:51 +0200

    Use event listeners in localStorage.js to handle device name change.

--- build/js/handleSaveWebauthnDevice.js
+++ build/js/handleSaveWebauthnDevice.js
@@ -2,17 +2,17 @@ const handleSaveWebauthnDevice = () => {
   const $ = jQuery.noConflict();
 
   /**
-   * Save Webauthn device name to localstorage on form submit.
+   * Triggers an event that updates
    */
   $('#webauthn-device-form').on('submit', (event) => {
     console.log('#webauthn-device-form::submit');
     const newDeviceName = $('.securitykey-name').val();
     const deviceObject = JSON.parse(window.localStorage.getItem('webauthn-device') || '');
     if (newDeviceName && deviceObject) {
-      window.localStorage.setItem('webauthn-device', JSON.stringify({
+      $(document).trigger('webauthn:update-device', {
         ...deviceObject,
         name: newDeviceName,
-      }));
+      });
     }
   });
 };
