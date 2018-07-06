47c2e2fb61204833abc366250ea8442427421750
Thay <thayanan@gmail.com>
2018-07-06 10:04:56 +0200

    Fix all btn scripts

--- build/js/register-device.js
+++ build/js/register-device.js
@@ -4,6 +4,7 @@ function hideInfo() {
     $(document).on('webauthn:register-success', (event, data) => {
         $('#webauthn-info').hide();
         $('#nextbtn').html("Lagre");
+        $('#registerSuccess').attr('action', "MinID-2D.html")
     })
 }
 
