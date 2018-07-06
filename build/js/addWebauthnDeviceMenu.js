f669337a81166d93ced042a320d870db996ab7c0
viktorfa <vikfand@gmail.com>
2018-07-06 10:40:52 +0200

    Make new Webauthn event for user presence prompt, and use that to make appropriate info box.

--- build/js/addWebauthnDeviceMenu.js
+++ build/js/addWebauthnDeviceMenu.js
@@ -1,25 +1,34 @@
 const addWebauthnDeviceMenu = () => {
-    const $ = jQuery.noConflict();
+  const $ = jQuery.noConflict();
 
-    $(document).on('webauthn:register-success', (event) => {
-        event.preventDefault();
-        $('#webauthn-added-device').toggle();
-        $('#add-webauthn-message').show();
-        $('#add-webauthn-message').text('Smartenheten er registrert');
-        $('#webauthn-hide-warning').toggle();
-        $('#webauthn-add-device-box').toggle();
-    });
+  $(document).on('webauthn:register-success', (event) => {
+    event.preventDefault();
+    $('#webauthn-added-device').toggle();
+    $('#add-webauthn-message').show();
+    $('#add-webauthn-message').text('Smartenheten er registrert');
+    $('#webauthn-hide-warning').toggle();
+    $('#webauthn-add-device-box').toggle();
+  });
+
+  $(document).on('webauthn:prompt-user-presence', (event) => {
+    event.preventDefault();
+    $('#webauthn-added-device').toggle();
+    $('#add-webauthn-message').show();
+    $('#add-webauthn-message').text('Trykk på smartenheten');
+    $('#webauthn-hide-warning').toggle();
+    $('#webauthn-add-device-box').toggle();
+  });
 
 
-    $('#undo-webauthn-device-btn').on('click', (event) => {
-        $(document).trigger('webauthn:remove-device', {});
-        $('#webauthn-hide-warning').toggle();
-        $('#add-webauthn-message').show();
-        $('#add-webauthn-message').text('Smartenheten er fjernet');
-        $('#webauthn-added-device').toggle();
-        $('#add-devices-controls').toggle();
-        $('#webauthn-add-device-box').toggle();
-    });
+  $('#undo-webauthn-device-btn').on('click', (event) => {
+    $(document).trigger('webauthn:remove-device', {});
+    $('#webauthn-hide-warning').toggle();
+    $('#add-webauthn-message').show();
+    $('#add-webauthn-message').text('Smartenheten er fjernet');
+    $('#webauthn-added-device').toggle();
+    $('#add-devices-controls').toggle();
+    $('#webauthn-add-device-box').toggle();
+  });
 };
 
 
--- build/js/webauthn.js
+++ build/js/webauthn.js
@@ -1,76 +1,78 @@
 const webauthn = () => {
-    const $ = jQuery.noConflict();
-    const name = $('#security-key-name').val() || $('#security-key-name').attr('placeholder');
+  const $ = jQuery.noConflict();
+  const name = $('#security-key-name').val() || $('#security-key-name').attr('placeholder');
 
-    $('#webAuthn').on('click', (event) => {
-        event.preventDefault();
-        console.log('test');
-        if (!navigator.credentials) {
-            window.alert('You need a browser that supports Webauthn');
-            return;
-        }
-        navigator.credentials.create(credentialCreationOptionsRegister)
-            .then((credentials) => {
-                console.log(credentials);
-                $(document).trigger('webauthn:register-success', { name, date: new Date() })
-            })
-            .catch(console.error);
-    });
+  $('#webAuthn').on('click', (event) => {
+    event.preventDefault();
+    console.log('test');
+    if (!navigator.credentials) {
+      window.alert('You need a browser that supports Webauthn');
+      return;
+    }
+    $(document).trigger('webauthn:prompt-user-presence');
+    navigator.credentials.create(credentialCreationOptionsRegister)
+      .then((credentials) => {
+        console.log(credentials);
+        $(document).trigger('webauthn:register-success', {name, date: new Date()})
+      })
+      .catch(console.error);
+  });
 
-    $('#webauthn-login').on('click', (event) => {
-        event.preventDefault();
-        if (!navigator.credentials) {
-            window.alert('You need a browser that supports Webauthn');
-            return;
-        }
-        navigator.credentials.get(credentialCreationOptionsLogin)
-            .then((credentials) => {
-                console.log(credentials);
-                $(document).trigger('webauthn:login-success')
-            })
-            .catch(console.error);
-    });
+  $('#webauthn-login').on('click', (event) => {
+    event.preventDefault();
+    if (!navigator.credentials) {
+      window.alert('You need a browser that supports Webauthn');
+      return;
+    }
+    $(document).trigger('webauthn:prompt-user-presence');
+    navigator.credentials.get(credentialCreationOptionsLogin)
+      .then((credentials) => {
+        console.log(credentials);
+        $(document).trigger('webauthn:login-success')
+      })
+      .catch(console.error);
+  });
 
-    $(document).on('webauthn:register-success', (event) => {
-        console.log('webauthn:register-success');
-        console.log(event);
-    });
+  $(document).on('webauthn:register-success', (event) => {
+    console.log('webauthn:register-success');
+    console.log(event);
+  });
 
-    const credentialCreationOptionsRegister = {
-        publicKey: {
-            rp: {
-                name: 'difi',
-            },
-            user: {
-                name: 'difi',
-                id: new Uint8Array(16),
-                displayName: 'difi',
-            },
-            pubKeyCredParams: [{
-                    type: 'public-key',
-                    alg: -7,
-                },
-                {
-                    type: 'public-key',
-                    alg: -36,
-                },
-                {
-                    type: 'public-key',
-                    alg: -257,
-                },
-            ],
-            challenge: new Uint8Array(16),
-            timeout: 60 * 1000,
+  const credentialCreationOptionsRegister = {
+    publicKey: {
+      rp: {
+        name: 'difi',
+      },
+      user: {
+        name: 'difi',
+        id: new Uint8Array(16),
+        displayName: 'difi',
+      },
+      pubKeyCredParams: [{
+        type: 'public-key',
+        alg: -7,
+      },
+        {
+          type: 'public-key',
+          alg: -36,
         },
-    };
-    const credentialCreationOptionsLogin = {
-        mediation: 'required',
-        publicKey: {
-            userVerification: 'required',
-            challenge: new Uint8Array(16),
-            timeout: 60 * 1000,
+        {
+          type: 'public-key',
+          alg: -257,
         },
-    };
+      ],
+      challenge: new Uint8Array(16),
+      timeout: 60 * 1000,
+    },
+  };
+  const credentialCreationOptionsLogin = {
+    mediation: 'required',
+    publicKey: {
+      userVerification: 'required',
+      challenge: new Uint8Array(16),
+      timeout: 60 * 1000,
+    },
+  };
 };
 
 
