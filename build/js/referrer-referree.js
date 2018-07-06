377832abe5bc8d6124f2278d68effc77b3e6c005
Itland <camp-vit@DIFI.no>
2018-07-06 14:03:25 +0200

    Close #132 - fix referrer-referree errors and bugs
    - Fixed typos (lacking window. before localStorage, relPath instead of
      relpath, currentUrl instead of url)
    - Added debug statements
    - Remove non-identifying part of url in both currentUrl and
      absReferreePath
    
    Also fixes #128
    
    TODO: add this pattern to other pages with back button behavior(?)

--- build/js/referrer-referree.js
+++ build/js/referrer-referree.js
@@ -4,14 +4,14 @@
     // getter for the localstorage referrer item
     const getReferrer = url => {
         const localStorageKey = "referrer:" + url;
-        return localStorage.getItem(localStorageKey);
+        return window.localStorage.getItem(localStorageKey);
     };
 
     // helper function: convert relative paths (relative to current URL) to
     // absolute paths
     const absolutePath = relPath => {
         let baseParts = window.location.href.split("/");
-        let relParts = relpath.split("/");
+        let relParts = relPath.split("/");
         
         baseParts.pop(); // remove current document (keep parent directories)
         
@@ -26,15 +26,22 @@
         
         return baseParts.join("/");
     }
+
+    // helper function: remove part of url following question mark, as this is
+    // not identifying to the page
+    const removeAfterQuestionMark = url => {
+        return url.split("?")[0]; // hax
+    }
     
     // current url (is absolute path)
-    const currentUrl = window.location.href;
+    const currentUrl = removeAfterQuestionMark(window.location.href);
     
     // create onclick listener to create a referrer object in localstorage for
     // any referrer elements
     $(".referrer").on("click", function(event) {
         // referrer/referrees must use absolute paths because it's unambiguous
-        const absReferreePath = absolutePath($(this).attr("href"));
+        const absReferreePath = removeAfterQuestionMark(absolutePath($(this).attr("href")));
+        console.log("Referrer clicked" + localStorageItem);
         
         $(document).trigger("redirect:create-referrer", {
             referrer: currentUrl, // is absolute already
@@ -44,9 +51,11 @@
     
     
     // set referree hrefs if there is a localStorageItem
-    const localStorageItem = getReferrer(url);
+    const localStorageItem = getReferrer(currentUrl);
     if (localStorageItem) {
+        console.log("Local storage element found for referree: was " + localStorageItem);
         $(".referree").attr('href', localStorageItem).on("click", (event) => {
+            console.log("Referree clicked");
             $(document).trigger('redirect:remove-referrer', currentUrl);
         });
     }
